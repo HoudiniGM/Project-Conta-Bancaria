bf91fbc823495c5d4d7f8a795a976f4ed92fd8c9
João Vitor Anacleto <joaovittormtm@gmail.com>
2022-10-30 16:14:44 -0300

    adicionar dados
    Criei a função para adicionar os dados.
    Antes disso, é validado se a senha é igual a confirmação da senha.

---
+++ script.js
@@ -0,0 +1,30 @@
+// VARIÁVEIS ----------------------------
+let listaDados = [];
+const form = document.getElementById('dados');
+
+
+
+// FUNÇÕES ------------------------------
+function criarDado(evento){
+    evento.preventDefault();
+
+    let dado = {
+        nome: document.getElementById('nome').value,
+        cpf: document.getElementById('cpf').value,
+        celular: document.getElementById('celular').value,
+        definirSenha: document.getElementById('definirSenha').value,
+        conta: Math.floor(1000 + Math.random() * 90000),
+        saldo: 0
+    }
+
+    if (dado.definirSenha === document.getElementById('confirmarSenha').value){
+        listaDados.push(dado);
+        alert(`Conta criado com sucesso!\nNúmero da conta: ${dado.conta}`)
+    } else {
+        alert('Senha incorreta');
+    }
+}
+
+
+// CHAMADAS DE FUNÇÕES -------------------
+form.addEventListener('submit', criarDado);
